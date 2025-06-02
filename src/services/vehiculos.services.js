eb225d663b10fbfb261d720ea7f030b4d1123ca3
JosueGomez99 <93223672+JosueGomez99@users.noreply.github.com>
2025-06-02 09:32:09 -0600

    Update vehiculos.services.js
    Update en backend de vehiculos

--- src/services/vehiculos.services.js
+++ src/services/vehiculos.services.js
@@ -5,15 +5,24 @@ class vehiculosServices {
   constructor() { }
 
   async find() {
-    const res = await models.vehiculo.findAll(); // Cambiado a singular
-    // Devuelve el array aunque esté vacío
+    // Solo los campos válidos de la tabla
+    const res = await models.vehiculo.findAll({
+      attributes: [
+        'id_vehiculo',
+        'placa',
+        'modelo',
+        'color',
+        'marca',
+        'idtipoVeh'
+      ]
+    });
     return res;
   }
   async findOne(id) {
-    const res = await models.vehiculo.findByPk(id); // Cambiado a singular
+    const res = await models.vehiculo.findByPk(id);
     if (!res) {
       throw new Error("Vehiculo no encontrado");
-  }
+    }
     return res;
   }
 
@@ -23,37 +32,31 @@ class vehiculosServices {
       if (error) {
         throw new Error(`Error de validación: ${error.details[0].message}`);
       }
-      const vehiculo = await models.vehiculo.create(value); // Cambiado a singular
+      const vehiculo = await models.vehiculo.create(value);
       return vehiculo;
     } catch (error) {
       console.error("Error al crear vehiculo", error.message);
       throw error;
     }
-
   }
   async update(id, data) {
-
     try{
       const { error, value } = vehiculosSchema.validate(data, { stripUnknown: true });
       if (error) {
         throw new Error(`Error de validación: ${error.details[0].message}`);
       }
-
-      const vehiculo = await this.findOne(id); // Cambiado a singular
+      const vehiculo = await this.findOne(id);
       if(!vehiculo){
         throw new Error("vehiculo no encontrado");
-
       }
       await vehiculo.update(value);
-
       return vehiculo;
     }catch(error){
       throw error;
     }
-    
   }
   async delete(id) {
-    const vehiculo = await this.findOne(id); // Cambiado a singular
+    const vehiculo = await this.findOne(id);
     await vehiculo.destroy();
     return { message: "Vehiculo eliminado" };
   }
